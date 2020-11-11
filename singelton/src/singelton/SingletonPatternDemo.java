9b0633d3278562995e2b7be0ead49b4e2a1dde1c
vaishnavi586 <machavolu1996@gmail.com>
2020-11-11 17:14:11 +0530

    added a code to design singleton pattern

---
+++ singelton/src/singelton/SingletonPatternDemo.java
@@ -0,0 +1,18 @@
+package singelton;
+
+public class SingletonPatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//illegal construct
+	      //Compile Time Error: The constructor SingleObject() is not visible
+	      //SingleObject object = new SingleObject();
+
+	      //Get the only object available
+	      SingleObject object = SingleObject.getInstance();
+
+	      //show the message
+	      object.showMessage();
+	}
+
+}
