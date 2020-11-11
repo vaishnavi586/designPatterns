5abc42875a8eee16a8c058a706c525de3ca6bf0e
vaishnavi586 <machavolu1996@gmail.com>
2020-11-11 17:28:58 +0530

    added a code to design facade pattern

---
+++ facadePattern/src/facadePattern/FacadePatternDemo.java
@@ -0,0 +1,14 @@
+package facadePattern;
+
+public class FacadePatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShapeMaker shapeMaker = new ShapeMaker();
+
+	      shapeMaker.drawCircle();
+	      shapeMaker.drawRectangle();
+	      shapeMaker.drawSquare();
+	}
+
+}
---
+++ facadePattern/src/facadePattern/Shape.java
@@ -0,0 +1,6 @@
+package facadePattern;
+
+public interface Shape {
+	void draw();
+
+}
